99766096c3b2a1f36fd3d4baa67bd384f1dd9a35
Jeffry Gonzalez <jeff@hypertheory.com>
2020-08-31 12:31:42 -0400

    before lunch monday final

--- src/app/components/todos/todo-entry/todo-entry.component.ts
+++ src/app/components/todos/todo-entry/todo-entry.component.ts
@@ -12,4 +12,12 @@ export class TodoEntryComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  add(descriptionEl: HTMLInputElement): void {
+    // - create a new TodoListItem
+    // - ?? Do Something with it.
+    console.log(descriptionEl.value);
+    // - Clear out the text in the textbox
+    descriptionEl.value = '';
+    descriptionEl.focus();
+  }
 }
